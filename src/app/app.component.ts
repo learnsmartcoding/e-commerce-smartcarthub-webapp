fd8acddbe50c277361d8a58a75d5ba5ea4ca0267
Learn Smart Coding <learnsmartcoding@gmail.com>
2024-03-27 21:24:55 -0400

    missing changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,6 +64,8 @@ export class AppComponent {
     private authService: MsalService,
     private msalBroadcastService: MsalBroadcastService,
     private loginService: AuthService,
+    private userProfileService:UserProfileService,
+    private orderService: OrderService,
     private router: Router, private titleService: Title
   ) {
     // set idle parameters
@@ -114,12 +116,12 @@ export class AppComponent {
         this.updateTitle(event.url);
       }
     });
-
     //this.handleLoginSuccess(); //this stores the redirecturl and handles it
 
     // right when the component initializes, start reset state and start watching
     this.reset();
- 
+    // this.getProfile();
+     this.getOrders();
     this.isIframe = window !== window.parent && !window.opener;
     this.setLoginDisplay();
 
@@ -341,6 +343,13 @@ export class AppComponent {
   ngOnDestroy(): void {
     this._destroying$.next(undefined);
     this._destroying$.complete();
+  }
+
+  getProfile(){
+    this.userProfileService.getUserProfile().subscribe(s=>console.log(JSON.stringify(s)));
+  }
+  getOrders(){
+    this.orderService.GetOrders().subscribe(data=>console.log(JSON.stringify(data)));
   }
 
   private updateTitle(url: string) {
--- src/app/shopping/shopping-routing.module.ts
+++ src/app/shopping/shopping-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   },
   {
     path: 'wishlist',
-    component: WishlistComponent
+    component: WishlistComponent,
+    canActivate: [canActivateGuard],
   },
   {
     path: 'checkout',
